8ed1ec0f377af16de1f447b7631763191128b99d
Rafael Carlos <shanglangjunior@hotmail.com>
2017-09-21 09:45:56 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoWS/ProjetoWS/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjetoWS
+{
+    public partial class MainPage : ContentPage
+    {
+        Cep cep;
+        public MainPage()
+        {
+            InitializeComponent();
+            cep = new Cep();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            atividade.IsVisible = true;
+            atividade.IsRunning = true;
+            //Validação de tamanho
+            if (txtCep.Text != null && txtCep.Text.Length == 8)
+            {
+                //Consumir serviço
+                //lblResultado.Text = await ServicoCEP.BuscaCEP(txtCep.Text);
+                Cep resultado = await ServicoCEP.BuscaCEP(txtCep.Text);
+
+                lblCep.Text = "Cep: " + resultado.cep;
+                lblEndereco.Text = "Endereço: " + resultado.logradouro;
+                lblComplemento.Text = "Complemento: " + resultado.complemento;
+                lblBairro.Text = "Bairro: " + resultado.bairro;
+                lblCidade.Text = "Cidade: " + resultado.localidade;
+                lblUf.Text = "UF: " + resultado.uf;
+                atividade.IsVisible = false;
+                atividade.IsRunning = false;
+
+            }
+            else
+            {
+                DisplayAlert("ERRO", "CEP informado é inválido!", "OK");
+
+                //lblResultado.Text = "O CEP informado é inválido!";
+                //lblResultado.TextColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ ProjetoWS/ProjetoWS/ServicoCEP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ProjetoWS
+{
+    public class ServicoCEP
+    {
+        public static string UrlBase = "https://viacep.com.br/ws/{0}/json/";
+
+        public async static Task<Cep> BuscaCEP(string cep)
+        {
+            string URL = string.Format(UrlBase, cep);
+            HttpClient http = new HttpClient();
+            //Esperando para poder obter o resultado.
+            string json = await http.GetStringAsync(URL);
+
+            //Deserializando objeto.
+            Cep objetoCEP = JsonConvert.DeserializeObject<Cep>(json);
+            //string resultado = string.Format("CEP: {0} " + "\n" +
+            //                                "UF: {1}" + "\n" +
+            //                                "Endereco: {2}  " + "\n" +
+            //                                "Cidade: {3}", objetoCEP.cep, objetoCEP.uf, objetoCEP.logradouro, objetoCEP.localidade);
+
+            //string resultado = string.Format("CEP: {0} " + "\n" +
+            //                                "UF: {1}" + "\n" +
+            //                                "Endereco: {2}  " + "\n" +
+            //                                "Cidade: {3}", objetoCEP.cep, objetoCEP.uf, objetoCEP.logradouro, objetoCEP.localidade);
+
+
+
+            return objetoCEP;
+        }
+    }
+}
